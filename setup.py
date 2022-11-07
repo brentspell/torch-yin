f75ed2a876e9e165ba277c3bf1e61d1ff1522d18
Brent M. Spell <bspell1@gmail.com>
2022-11-07 07:23:49 -0500

    allow numpy 1.19 for python 3.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name="torch-yin",
-    version="0.1.2",
+    version="0.1.3",
     description="Yin pitch estimator for PyTorch",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
@@ -14,7 +14,7 @@ setup(
     author_email="brent@brentspell.com",
     packages=setuptools.find_packages(),
     setup_requires=[],
-    install_requires=["numpy>=1.20", "torch>=1.0"],
+    install_requires=["numpy>=1.19", "torch>=1.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
